ad4cbf8cb5129c46629abc90832ff0decabe87a7
NikitaS <dekgold@gmail.com>
2024-06-05 22:37:21 +0300

    12 спринт

--- app/src/main/java/com/example/myplaylistmaker/SearchHistoryAdapter.kt
+++ app/src/main/java/com/example/myplaylistmaker/SearchHistoryAdapter.kt
@@ -4,24 +4,29 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TrackAdapter(
-    private val data: List<Track>,
+class SearchHistoryAdapter(
+    private var historyList: ArrayList<Track>,
     private val onTrackSelected: (Track) -> Unit
-) : RecyclerView.Adapter<TrackViewHolder> () {
+) : RecyclerView.Adapter<TrackViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.track_items, parent, false)
         return TrackViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
-        val track = data[position]
-        holder.bind(track)
+        holder.bind(historyList[position])
         holder.itemView.setOnClickListener {
-            onTrackSelected(track)
+            onTrackSelected(historyList[position])
         }
     }
 
     override fun getItemCount(): Int {
-        return data.size
+        return historyList.size
+    }
+
+    fun updateHistoryList(newHistoryList: ArrayList<Track>) {
+        historyList = newHistoryList
+        notifyDataSetChanged()
     }
 }
