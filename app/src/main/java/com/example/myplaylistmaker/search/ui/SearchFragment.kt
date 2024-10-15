a446ce9dcbb5af5da661801bb61acad2426cdd2d
NikitaS <dekgold@gmail.com>
2024-10-15 22:06:30 +0300

    20 спринт ( Исправления 2)

--- app/src/main/java/com/example/myplaylistmaker/search/ui/SearchFragment.kt
+++ app/src/main/java/com/example/myplaylistmaker/search/ui/SearchFragment.kt
@@ -155,6 +155,7 @@ class SearchFragment : Fragment() {
                         }
                     }
                 } else {
+                    updateTrackList(null)
                     updateHistoryVisibility()
                 }
             }
